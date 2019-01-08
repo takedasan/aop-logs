897161dbdb7de353b868d8decf102ede07e116a4
takedasan <takedasan593@gmail.com>
2019-01-09 00:06:02 +0900

    Add security logs

--- src/main/kotlin/jp/takeda/aoplogs/Aspect.kt
+++ src/main/kotlin/jp/takeda/aoplogs/Aspect.kt
@@ -3,6 +3,8 @@ package jp.takeda.aoplogs
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
 import org.springframework.stereotype.Component
 
 
@@ -12,6 +14,10 @@ class AspectLog {
 
     @Before("execution(* jp.takeda.aoplogs.*.*(..))")
     fun before(joinPoint: JoinPoint) {
-        println("beforeだよー。")
+        val principal = SecurityContextHolder.getContext().authentication.principal
+
+        if (principal is User) {
+            println("${principal.username}:beforeだよー")
+        }
     }
 }
